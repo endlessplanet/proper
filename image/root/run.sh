89960a9d00075aa16e36cc5aa6ab84db3d60374c
root <emory.merryman@gmail.com>
2017-08-25 12:10:23 +0000

    add docker group

--- image/root/run.sh
+++ image/root/run.sh
@@ -3,12 +3,13 @@
 dnf update --assumeyes &&
     dnf install --assumeyes sudo &&
     adduser user &&
+    groupadd docker &&
+    usermod -a -G docker user &&
     cp /opt/docker/user.sudo /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     dnf install --assumeyes dnf-plugins-core &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf update --assumeyes &&
     dnf install --assumeyes dnf docker-common docker-latest &&
-    usermod -a -G docker user &&
     dnf update --assumeyes &&
     dnf clean all
